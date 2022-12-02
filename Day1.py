c62e3315013a929c86364c4dc22144a1f6dc9f11
Andrea Zagar <azagar@esri.ca>
2022-12-02 09:57:06 -0800

    Day 1 complete

---
+++ Day1.py
@@ -0,0 +1,21 @@
+''' GOAL: Find elf with most calories '''
+
+# Store counts
+top_calories = []
+current_total = 0
+
+# Read input file
+with open('Input/Day1.txt') as f:
+    while True:
+        line = f.readline()
+        if not line: break
+        elif line == '\n':
+            top_calories.append(current_total)
+            current_total = 0
+        else:
+            current_total += int(line.strip())
+
+# Sort list and print answers
+top_calories.sort()
+print("Top 1 calories: " + str(top_calories[-1]))
+print("Top 3 calories sum: " + str(sum(top_calories[-3:])))
